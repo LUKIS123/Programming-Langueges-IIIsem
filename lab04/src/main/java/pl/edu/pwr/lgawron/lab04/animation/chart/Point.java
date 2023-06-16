970790432caf5bf507a76245109a58872f0458ad
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2023-06-16 20:12:58 +0200

    clean up + readme

--- lab04/src/main/java/pl/edu/pwr/lgawron/lab04/animation/chart/Point.java
+++
@@ -1,4 +0,0 @@
-package pl.edu.pwr.lgawron.lab04.animation.chart;
-
-public record Point(double x, double y) {
-}
--- lab05/src/main/java/pl/edu/pwr/lgawron/lab05/actors/RunnableActor.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.pwr.lgawron.lab05.actors;
-
-public interface RunnableActor extends Runnable {
-    void run();
-
-    void setExit(boolean exit);
-}
